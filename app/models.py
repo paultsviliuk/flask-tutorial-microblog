6a910697a799be3ad0329028b397e2bb4d1c17ea
angmar33 <paultsvilyuk@gmail.com>
2018-10-28 21:00:14 +0200

    tests added

--- app/models.py
+++ app/models.py
@@ -52,10 +52,11 @@ class User(UserMixin, db.Model):
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        return Post.query.join(
+        followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-            followers.c.follower_id == self.id).order_by(
-            Post.timestamp.desc())
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 
 @login.user_loader
