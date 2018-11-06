aa2bcde6eb15a1b593ad72f88913ff75fe602d5c
angmar33 <paultsvilyuk@gmail.com>
2018-11-06 14:31:33 +0200

    dates and times added

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 
 app = Flask(__name__)
@@ -18,6 +19,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 from app import routes, models, errors
